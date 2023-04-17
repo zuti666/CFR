bc1c3b824d312c85c9cb43e50399ad8002aca473
YingFei <38252018+zuti666@users.noreply.github.com>
2023-04-17 21:23:16 +0800

    上传代码

---
+++ Kuhn-Poker/test.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+"""
+@author     : zuti
+@software   : PyCharm
+@file       : test.py
+@time       : 2022/11/24 16:40
+@desc       ：
+
+"""
+import numpy as np
+
+n = np.array([4,2,3])
+n2 = np.array([4,2,3])
+print(n*n2)
+
+
